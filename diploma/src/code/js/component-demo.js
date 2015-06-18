13a4306b312530928044cdc5f4eac0851c585614
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2015-06-18 19:39:31 +0300

    Update React section

---
+++ diploma/src/code/js/component-demo.js
@@ -0,0 +1,14 @@
+var InputComponent = React.createClass({
+  getInitialState: function() {
+    return {value: this.props.val};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  render: function(){
+    return (
+      <input type="text" value={this.state.value} onChange={this.handleChange}/>
+    );
+  }
+});
+React.render(<FormBox val="Default value" />, document.getElementById('content'));
